36da170309b07502914f146bb53f8ec54775902c
Jonathan Attram <codinghub242@gmail.com>
2025-07-21 13:22:15 +0000

    Update

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter,Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+// Lusitana is a serif font, used for headings or special text
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
